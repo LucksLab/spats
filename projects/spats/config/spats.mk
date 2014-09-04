a6811a8117e317c1b17e056d11f1e644fd9a5521
jblucks@berkeley.edu <jblucks@berkeley.edu@e53bd72b-32bf-4dec-aa00-2f942a4fcc91>
2014-09-04 19:41:39 +0000

    attempting to have a dist_SCRIPTS target in make process that does not strip .py

--- projects/spats/config/spats.mk
+++ projects/spats/config/spats.mk
@@ -3,7 +3,7 @@
 ##-- install-exec-hook
 ##   execute this custom rule after the global install-exec rule
 ##
-install-exec-hook: install-bin-scripts-hook
+install-exec-hook: install-bin-scripts-hook install-scripts-hook
 
 ##-- uninstall-hook
 ##   execute this custom rule after the global uninstall rule
@@ -23,6 +23,53 @@ install-bin-scripts-hook: $(dist_bin_SCRIPTS) $(bin_SCRIPTS)
 	@list='$(dist_bin_SCRIPTS) $(bin_SCRIPTS)'; for p in $$list; do \
           f=`echo "$$p" | sed 's|^.*/||;$(transform)'`; \
           b=`echo "$$p" | sed 's|^.*/||;s|.[^.]*$$||;$(transform)'`; \
+          if test -f "$(DESTDIR)$(bindir)/$$f"  &&  test "$$f" != "$$b"; then \
+            $(INSTALL_SCRIPT) "$(DESTDIR)$(bindir)/$$f" "$(DESTDIR)$(bindir)/$$b" \
+            || exit 1; \
+            case "$$p" in \
+              *.py) \
+                echo " configuring python '$$b'"; \
+                echo '#!$(PYTHON)' > "$(DESTDIR)$(bindir)/$$b"; \
+                sed -e '1 {s|^#!.*$$||;}' \
+                    "$(DESTDIR)$(bindir)/$$f" >> "$(DESTDIR)$(bindir)/$$b" \
+                || exit 1; \
+                ;; \
+              *.sh) \
+                echo " configuring shell '$$b', using #!$(SPATS_SHELL)"; \
+                echo '#!$(SPATS_SHELL)' > "$(DESTDIR)$(bindir)/$$b"; \
+                sed -e '1 {s|^#!.*$$||;}' \
+                    -e 's|BINDIR[[:space:]]*=.*|BINDIR=$(bindir)|' \
+                    "$(DESTDIR)$(bindir)/$$f" >> "$(DESTDIR)$(bindir)/$$b" \
+                || exit 1; \
+                ;; \
+			  *.pl) \
+	                echo " configuring perl '$$b'"; \
+	                echo '#!$(PERL)' > "$(DESTDIR)$(bindir)/$$b"; \
+	                sed -e '1 {s|^#!.*$$||;}' \
+	                    "$(DESTDIR)$(bindir)/$$f" >> "$(DESTDIR)$(bindir)/$$b" \
+	                || exit 1; \
+	                ;; \
+              *) \
+                echo " configuring '$$b'"; \
+                cp "$(DESTDIR)$(bindir)/$$f" "$(DESTDIR)$(bindir)/$$b" \
+                ;; \
+            esac; \
+            rm -f "$(DESTDIR)$(bindir)/$$f"; \
+          else :; fi; \
+        done
+
+##-- install-scripts-hook
+##   takes list of scripts from $(dist_SCRIPTS) and:
+##   1) locates the installed script
+##   2) builds them with the below sed commands to assure the shebang and
+##   include directives point to the correct locations, and that variable
+##   substitution is performed as expected
+##
+install-scripts-hook: $(dist_SCRIPTS) $(bin_SCRIPTS)
+	@$(NORMAL_INSTALL)
+	@list='$(dist_bin_SCRIPTS) $(bin_SCRIPTS)'; for p in $$list; do \
+          f=`echo "$$p" | sed 's|^.*/||;$(transform)'`; \
+          b=`echo "$$p" | sed 's|^.*/||;$(transform)'`; \
           if test -f "$(DESTDIR)$(bindir)/$$f"  &&  test "$$f" != "$$b"; then \
             $(INSTALL_SCRIPT) "$(DESTDIR)$(bindir)/$$f" "$(DESTDIR)$(bindir)/$$b" \
             || exit 1; \
