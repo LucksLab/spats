ba7d893e35bbf10b62e021f4a8fb4c6feedb1907
jblucks@berkeley.edu <jblucks@berkeley.edu@e53bd72b-32bf-4dec-aa00-2f942a4fcc91>
2014-09-04 11:37:57 +0000

    Updating documentation in preparation for v0.8.0 release

--- projects/spats/src/spats.py
+++ projects/spats/src/spats.py
@@ -662,7 +662,7 @@ def write_sam_header(read_params, sam_file):
     print >> sam_file, "@PG\tID:Spats\tVN:%s\tCL:%s" % (get_version(), run_cmd)
 
 def get_version():
-   return "0.0.1"
+   return "0.8.0"
 
 # From http://www.dalkescientific.com/writings/NBN/parsing.html
 class FastaRecord(object):
