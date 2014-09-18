448f68d8758b532ad82affc687bcfc74c68adee8
kew222 <kew222@cornell.edu>
2014-09-18 11:45:29 -0400

    Major update for the cutadapt scripts.  This commit includes a more stable version of the adapter_trimmer algorithm that passes the basic test fastq files.

---
+++ projects/spats/fastq_revcomp.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+#Brief module for reverse-complementing a fastq file with fastx_reverse_complement
+
+import os
+
+
+def rev_comp(inputfile, output_revcomp='blank'):
+    
+    if output_revcomp == 'blank':
+        output_revcomp = os.path.splitext(inputfile)[0] + '_rc.fq'
+    
+   #Uses the given input and output filenames to make a reverse complement, outputs file name
+    os.system("fastx_reverse_complement -Q33 -i {0} -o {1}".format(inputfile,output_revcomp))
+
+
+def main(argv=None,):
+   pass 
+    
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
