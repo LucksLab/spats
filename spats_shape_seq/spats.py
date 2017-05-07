86916eb634307b6c062b817e5967aa47efd2f3dd
jjb <jjb.luckslab@gmail.com>
2017-05-07 09:59:14 -0700

    refactor core algorithm into PairProcessor

--- spats_shape_seq/spats.py
+++ spats_shape_seq/spats.py
@@ -182,6 +182,7 @@ from db import PairDB
 from mask import Mask, longest_match
 from pair import Pair
 from parse import FastFastqParser, fasta_parse
+from processor import PairProcessor
 from profiles import Profiles
 from target import Targets
 from util import _warn, _debug, Counters, reverse_complement, string_match_errors
@@ -196,6 +197,7 @@ class Spats(object):
         #: reference to the :class:`.config.SpatsConfig` instance
         self.config = spats_config
 
+        self.__processor = None
         self._targets = Targets()
         self._masks = []
         self._maskSize = 0
@@ -205,6 +207,14 @@ class Spats(object):
         self.writeback_results = False
         self.result_set_name = None
         self.only_new_results = False
+
+
+    @property
+    def _processor(self):
+        if not self.__processor:
+            self.__processor = PairProcessor(self._targets, self._masks)
+        return self.__processor
+
 
     def addMasks(self, *args):
         """Pass the masks used for R1 handles to classify treated/untreated samples.
@@ -247,89 +257,6 @@ class Spats(object):
             target.index()
             target.minimum_match_length = 1 + target.longest_self_match()
 
-
-    @property
-    def _adapter_t_rc(self):
-        if not self._adapter_t_rc_cached:
-            self._adapter_t_rc_cached = reverse_complement(spats_config.adapter_t)
-        return self._adapter_t_rc_cached
-
-    def _match_mask(self, pair):
-        seq = pair.r1.original_seq
-        if len(seq) <= self._maskSize:
-            _warn("sequence {} is too short for masking".format(seq))
-            return
-        for mask in self._masks:
-            if mask.matches(seq):
-                pair.set_mask(mask)
-                return
-
-    def _find_matches(self, pair):
-        # use R1 to determine which target
-        target = pair.r1.find_in_targets(self._targets, reverse_complement = True)
-        if isinstance(target, list):
-            _debug("dropping pair due to multiple R1 match")
-            pair.failure = "multiple R1 match"
-            self.counters.multiple_R1_match += pair.multiplicity
-            return
-        if target:
-            pair.target = pair.r2.find_in_targets(self._targets, force_target = target)
-        _debug([pair.r1.match_start, pair.r1.match_len, pair.r1.match_index, "--", pair.r2.match_start, pair.r2.match_len, pair.r2.match_index])
-
-    def _trim_adapters(self, pair):
-
-        # if we're here, we know that R2 hangs over the right edge. first, figure out by how much
-
-        r2_seq = pair.r2.subsequence
-        r2_start_in_target = pair.r2.match_index - pair.r2.match_start
-        r2_len_should_be = pair.target.n - r2_start_in_target
-        r2_length_to_trim = pair.r2.seq_len - r2_len_should_be
-        pair.r2.trim(r2_length_to_trim)
-        _debug("R2 trim: {}, {}, {}".format(r2_start_in_target, r2_len_should_be, r2_length_to_trim))
-
-        if spats_config.minimum_adapter_len and r2_length_to_trim - 4 < spats_config.minimum_adapter_len:
-            _debug("  !! v102 minimum adapter len {}".format(r2_length_to_trim - 4))
-            return False
-
-        if r2_length_to_trim <= 4:
-            # TODO: should we verify that this matches RC of R1 handle, and register errors for bp that don't?
-            # for now, just ignore this part
-            # also, this means that there's nothing to trim from R1, so we're done
-            return True
-
-        # find out how good of a match the end of R2 is for adapter_t_rc
-        r2_adapter_match = r2_seq[4-r2_length_to_trim:]
-        pair.r2.adapter_errors = string_match_errors(r2_adapter_match, self._adapter_t_rc)
-        _debug("  check = {}, errors = {}".format(r2_adapter_match, pair.r2.adapter_errors))
-        if len(pair.r2.adapter_errors) > spats_config.allowed_adapter_errors:
-            return False
-
-        # now, same thing on r1 (smaller trim b/c of no handle, hence -4)
-        r1_seq = pair.r1.subsequence
-        r1_length_to_trim = r2_length_to_trim - 4
-        r1_adapter_match = r1_seq[-r1_length_to_trim:]
-        r1_match_trimmed = pair.r1.trim(r1_length_to_trim, reverse_complement = True)
-        pair.r1.adapter_errors = string_match_errors(r1_adapter_match, spats_config.adapter_b)
-        _debug("  R1 check = {}, errors = {}".format(r1_adapter_match, pair.r1.adapter_errors))
-        if len(pair.r1.adapter_errors) > spats_config.allowed_adapter_errors:
-            return False
-
-        if r1_match_trimmed:
-            # ok, we trimmed down our R1 due to adapters. need to see if that means the leftover matches
-            # multiple targets; if so, need to reject this pair.
-            target = pair.r1.find_in_targets(self._targets, reverse_complement = True, min_length_override = pair.r1.match_len)
-            if not target or isinstance(target, list):
-                # note that target may be None if pair.r1.match_len is less than the index word length
-                _debug("dropping pair due to multiple R1 match after adapter trim")
-                pair.target = None
-                pair.failure = "multiple R1 match"
-                self.counters.multiple_R1_match += pair.multiplicity
-                return False
-
-        _debug("successful adapter trim of {}/{} bp from R1/R2".format(pair.r1._rtrim, pair.r2._rtrim))
-
-        return True
-
     def process_pair(self, pair):
         """Used process a single :class:`.pair.Pair`. Typically only used for debugging or analysis of specific cases.
 
@@ -339,7 +266,7 @@ class Spats(object):
         _debug("> processing " + pair.identifier + "\n  --> " + pair.r1.original_seq + " , " + pair.r2.original_seq)
         _debug("  rc(R1): {}".format(pair.r1.reverse_complement))
         try:
-            self._process_pair(pair)
+            self._processor.process_pair(pair)
             if pair.failure:
                 _debug(pair.failure)
             else:
@@ -349,75 +276,6 @@ class Spats(object):
             print "**** Error processing pair: {} / {}".format(pair.r1.original_seq, pair.r2.original_seq)
             raise
 
-    #@profile
-    def _process_pair(self, pair):
-
-        if not spats_config.allow_indeterminate  and  not pair.is_determinate():
-            pair.failure = "indeterminate sequence failure"
-            self.counters.indeterminate += pair.multiplicity
-            return
-
-        self._match_mask(pair)
-        if not pair.mask:
-            self.counters.mask_failure += pair.multiplicity
-            pair.failure = "mask failure"
-            return
-
-        self._find_matches(pair)
-        if not pair.matched:
-            self.counters.unmatched += pair.multiplicity
-            pair.failure = "no match"
-            return
-
-        # this is where R2 should start (if not a complete match, then r2.match_start will be > 0)
-        r2_start_in_target = pair.r2.match_index - pair.r2.match_start
-        if r2_start_in_target < 0:
-            pair.failure = "R2 to left of site 0 failure on R2 for: {}".format(pair.identifier)
-            self.counters.left_of_target += pair.multiplicity
-            return
-        elif r2_start_in_target + pair.r2.original_len <= pair.target.n:
-            # we're in the middle -- no problem
-            pass
-        elif not self._trim_adapters(pair):
-            # we're over the right edge, and adapter trimming failed
-            pair.failure = pair.failure or "adapter trim failure"
-            self.counters.adapter_trim_failure += pair.multiplicity
-            return
-        else:
-            # we're at the right and trimming went ok, cool
-            self.counters.adapter_trimmed += pair.multiplicity
-
-        if pair.r2.match_len == pair.r2.seq_len  and  pair.r1.match_len == pair.r1.seq_len:
-            # everything that wasn't adapter trimmed was matched -- nothing to do
-            pass
-        else:
-            # set the match to be the rest of the (possibly trimmed) sequence, and count errors
-            pair.r1.match_to_seq(reverse_complement = True)
-            pair.r2.match_to_seq()
-            target_seq = pair.target.seq
-            pair.r1.match_errors = string_match_errors(pair.r1.reverse_complement, target_seq[pair.r1.match_index:])
-            pair.r2.match_errors = string_match_errors(pair.r2.subsequence, target_seq[pair.r2.match_index:])
-
-            if max(len(pair.r1.match_errors), len(pair.r2.match_errors)) > spats_config.allowed_target_errors:
-                if pair.r1.match_errors:
-                    _debug("R1 errors: {}".format(pair.r1.match_errors))
-                if pair.r2.match_errors:
-                    _debug("R2 errors: {}".format(pair.r2.match_errors))
-                pair.failure = "match errors failure"
-                self.counters.match_errors += pair.multiplicity
-                return
-
-        n = pair.target.n
-        assert(pair.matched and pair.left >= 0 and pair.left <= n)
-
-        # NOTE: this might change later due to "starts"
-        if pair.right != n:
-            pair.failure = "R1 right edge failure: {} - {}, n={}".format(pair.left, pair.right, n)
-            self.counters.r1_not_on_right_edge += pair.multiplicity
-            return
-
-        pair.register_count()
-        self.counters.processed_pairs += pair.multiplicity
 
     def _memory_db_from_pairs(self, data_r1_path, data_r2_path):
         start = time.time()
@@ -517,17 +375,17 @@ class Spats(object):
             thd.start()
         _debug("created {} workers".format(num_workers))
 
-        msg = "Retrieving unique pairs..."
         if self.only_new_results:
             db_iter = pair_db.unique_pairs_with_counts_and_no_results(self.result_set_id, batch_size = 32768)
         elif spats_config._process_all_pairs:
-            msg = "Using all_pairs..."
+            if not spats_config.quiet:
+                print "Using all_pairs..."
             db_iter = pair_db.all_pairs(batch_size = 32768)
         else:
             db_iter = pair_db.unique_pairs_with_counts(batch_size = 32768)
 
         if not spats_config.quiet:
-            print msg
+            print "Processing pairs..."
 
         for pair_info in db_iter:
             if not spats_config._process_all_pairs:
