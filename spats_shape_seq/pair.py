dbaec123c55821e1f0170c09326a818d391665ef
jim brink <jim@branchfire.com>
2018-06-27 19:47:06 -0700

    handle mutation quality with rtrim case

--- spats_shape_seq/pair.py
+++ spats_shape_seq/pair.py
@@ -61,9 +61,9 @@ class Pair(object):
         for mut in self.mutations:
             q1 = q2 = None
             if mut < r2_start + seq_len + 1:
-                q1 = ord(self.r2.quality[mut - r2_start - 1])
+                q2 = ord(self.r2.quality[mut - r2_start - 1])
             if mut > r1_start:
-                q2 = ord(self.r1.quality[::-1][mut - r1_start - 1])
+                q1 = ord(self.r1.quality[::-1][mut - r1_start + self.r1._rtrim - 1])
             # note: this assumes agreement. TODO: handle disagreement, xref check_overlap below
             q = max(q1, q2) if (q1 and q2) else (q1 if q1 else q2)
             if q < minimum_quality_score:
