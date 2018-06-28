af677567b9705f8b117994b15d2292ace743c8d7
Stephen Hockema <steve@496code.com>
2018-06-27 21:08:07 -0700

    Added max_indices param to string_find_errors().

--- spats_shape_seq/util.py
+++ spats_shape_seq/util.py
@@ -31,8 +31,9 @@ def string_match_errors(substr, target_str, max_errors = None):
                 return errors
     return errors
 
-def _slow_string_find_errors(substr, target_str, max_errors = 2):
+def _slow_string_find_errors(substr, target_str, max_errors = 2, max_indices = 2):
     assert(max_errors > 1)
+    assert(max_indices > 0)
     if max_errors >= len(substr):
         raise Exception("more errors allowed than len of substr being sought!")
     result = []
@@ -48,12 +49,14 @@ def _slow_string_find_errors(substr, target_str, max_errors = 2):
             s += 1
         if s == ls:
             result.append(t)
+            if len(result) >= max_indices:
+                break
     return result
 
-def string_find_errors(substr, target_str, max_errors = 0):
-
-    if max_errors > 1:
-        return _slow_string_find_errors(substr, target_str, max_errors)
+def string_find_errors(substr, target_str, max_errors = 0, max_indices = 2):
+    assert(max_errors >= 0)
+    assert(max_indices > 0)    # TAI:  consider using max_indices == -1 to return all
+    me = min(max_errors, 1)
 
     half_sublen = (len(substr) >> 1)
     first_half = substr[:half_sublen]
@@ -61,27 +64,32 @@ def string_find_errors(substr, target_str, max_errors = 0):
 
     candidates = []
     index = 0
-    while True:
+    while len(candidates) < max_indices:
         index = target_str.find(first_half, index)
         if -1 == index or index + len(substr) > len(target_str):
             break
-        errors = string_match_errors(second_half, target_str[index + len(first_half):], max_errors + 1)
-        if len(errors) <= max_errors:
+        errors = string_match_errors(second_half, target_str[index + len(first_half):], me + 1)
+        if len(errors) <= me:
             candidates.append(index)
         index = index + 1
 
     index = 0
-    while True:
+    while len(candidates) < max_indices:
         index = target_str.find(second_half, index)
         if -1 == index:
             break
         if index - len(first_half) >= 0:
-            errors = string_match_errors(first_half, target_str[index - len(first_half):], max_errors + 1)
-            if len(errors) <= max_errors:
+            errors = string_match_errors(first_half, target_str[index - len(first_half):], me + 1)
+            if len(errors) <= me:
                 candidates.append(index - len(first_half))
         index = index + 1
 
-    return sorted(list(set(candidates)))
+    result = sorted(list(set(candidates)))
+
+    if len(result) < max_indices  and  max_errors > 1:
+        result += _slow_string_find_errors(substr, target_str, max_errors, max_indices - len(result))
+
+    return result
 
 class Colors(object):
 
