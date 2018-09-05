ed0330e9fc9cf949c3337f59f69f2fcbccc1f87f
Stephen Hockema <steve@496code.com>
2018-09-05 17:02:37 -0400

    Experimenting with TestResult object.  Still not quite right.

--- spats_shape_seq/tests/test_harness.py
+++ spats_shape_seq/tests/test_harness.py
@@ -1,20 +1,42 @@
+import sys
 import unittest
 import simplejson     # Use so don't have to bother with unicode->str 
 from spats_shape_seq import Spats
 from spats_shape_seq.pair import Pair
 
 
+
 class TestHarness:
+    class SpatsTestResults(unittest.TextTestResult):
+        # Taken from legacy python unittest
+        class WritelnDecorator:
+            """Used to decorate file-like objects with a handy 'writeln' method"""
+            def __init__(self, stream):
+                self.stream = stream
+            def __getattr__(self, attr):
+                return getattr(self.stream,attr)
+            def writeln(self, arg = None):
+                if arg: self.write(arg)
+                self.write('\n') # text-mode streams translate to \r\n if needed
+        def __init__(self, outer):
+            super(TestHarness.SpatsTestResults, self).__init__(TestHarness.SpatsTestResults.WritelnDecorator(sys.stdout), True, 1)
+        # TAI:  can override addFailure(self, test, err) and # addError(self, test, err) here if we want 
+
     def __init__(self, testfile = "tests.json"):
+        self.test_results = self.SpatsTestResults(self)
         with open(testfile) as TF:
             self.tests = simplejson.load(TF)
 
     def run_testsets(self):
         for testset_dict in self.tests["tests"]:
-            self.TestSet(self, testset_dict).run()   # TODO:  use a unittest.TestResult object
+            self.TestSet(self, testset_dict).run(self.test_results)   # TODO:  use a unittest.TestResult object
 
     def print_stats(self):
-        pass # TODO:  use a unittest.TestResult object
+        print("\n\nSUMMARY")
+        print("Tests Sets Run:  {}".format(self.test_results.testsRun))
+        print("All Test Sets Passed?  {}".format(self.test_results.wasSuccessful()))
+        if not self.test_results.wasSuccessful():
+            self.test_results.printErrors()
 
 
     # TAI:  Use a unittest.TestSuite object
@@ -34,7 +56,7 @@ class TestHarness:
                     self.algorithms = value
                 else:
                     setattr(self.spats.run, key, value)
-            for key, value in self.testset_dict['run_opts'].iteritems():
+            for key, value in self.testset_dict.get('run_opts', {}).iteritems():
                 if (key == 'algorithms'):
                     self.algorithms = value
                 else:
@@ -54,9 +76,8 @@ class TestHarness:
                     try:
                         self._run_case(case)
                     except Exception as e:
+                        # TODO:  This isn't right.  If we catch here, it wont update the test results.  (but if we don't, it aborts in the middle of the test cases for this set.  --> need to use a suite.
                         print("exception caught on test '{}': {}".format(case['id'], e))
-
-
 
         def _run_case(self, case):
             #print("running case {} ({})...".format(case['id'], case.get('comment', '')))
@@ -98,8 +119,6 @@ class TestHarness:
             # TODO:  not sure about this one...
             if 'pair.target' in expects:
                 self.assertEqual(pair.target, expects['pair.target'], msg + "pair.target={} != expect.pair.target={}".format(pair.target, expects['pair.target']))
-            # TAI:  when test fails, diagram it
-            # TODO:  increment stats in self.outer and/or unittest.TestResult object
 
 
 
