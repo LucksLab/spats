b7c47c817b60769375fb4a1d36364c1a46d0f83e
jim brink <jbrink@local>
2020-03-04 00:13:47 +0000

    verison bump

--- spats_shape_seq/__init__.py
+++ spats_shape_seq/__init__.py
@@ -55,7 +55,7 @@ This can be later loaded using the :meth:`.spats.Spats.load` method.
 from spats import Spats
 
 
-_VERSION = "1.9.44"
+_VERSION = "2.0.0"
 _PRODUCTION = True
 _PUBLIC_RELEASE = True
 version = _VERSION + (" Public" if _PUBLIC_RELEASE else " Private") + (" Production" if _PRODUCTION else " Beta")
